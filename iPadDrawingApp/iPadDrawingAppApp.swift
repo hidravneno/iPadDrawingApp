d227755a00f54aa21ef1f1131f8185a93d6849f3
aramburo reyes francisco eduardo <aramburoeduardo972@gmail.com>
2025-12-15 19:00:26 -0800

    Initial Commit

---
+++ iPadDrawingApp/iPadDrawingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPadDrawingAppApp.swift
+//  iPadDrawingApp
+//
+//  Created by francisco eduardo aramburo reyes on 15/12/25.
+//
+
+import SwiftUI
+
+@main
+struct iPadDrawingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
